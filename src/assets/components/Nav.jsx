7dba758be4145f59ef35642cf56b4744dd226298
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2025-05-26 23:55:31 +0200

    image errors testing.

--- src/assets/components/Nav.jsx
+++ src/assets/components/Nav.jsx
@@ -4,8 +4,8 @@ import { NavLink } from 'react-router-dom';
 const Nav = () => {
   return (
     <nav>
-      <NavLink to="/dashboard" className="ventixe-logo-link">
-        <img src="/ventixe.svg" alt="Ventixe logo" className="ventixe-logo" />
+      <NavLink to="/home" className="ventixe-logo-link">
+        <img src="/public/ventixe.svg" alt="Ventixe logo" className="ventixe-logo" />
         <p className='strangle'>Ventixe</p>
       </NavLink>
       <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : '')}>
@@ -16,7 +16,6 @@ const Nav = () => {
         <i className="bi bi-ticket-perforated"></i>
         <span className='strangle'>Events</span>
       </NavLink>
-      
     </nav>
   );
 };
